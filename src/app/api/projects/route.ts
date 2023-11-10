483de8831ffcc1bf5bd6601f5774b6817e89563f
Jeremias Villane <vil.jeremias@gmail.com>
2023-11-09 22:40:50 -0300

    added projects endpoint

---
+++ src/app/api/projects/route.ts
@@ -0,0 +1,71 @@
+import { Project, connectToDB } from "@/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+  try {
+    await connectToDB();
+    const extractData = await Project.find({});
+
+    if (extractData) {
+      return NextResponse.json({
+        statusCode: 200,
+        error: null,
+        data: extractData,
+      });
+    } else {
+      return NextResponse.json({
+        statusCode: 400,
+        error: {
+          message: "Something went wrong. Please try again",
+        },
+        data: null,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json({
+      statusCode: 500,
+      error: {
+        message: "Something went wrong. Please try again",
+      },
+      data: null,
+    });
+  }
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectToDB();
+    const extractData = await req.json();
+    const saveData = await Project.create(extractData);
+
+    if (saveData) {
+      return NextResponse.json({
+        statusCode: 201,
+        error: null,
+        data: "Data saved successfully",
+      });
+    } else {
+      return NextResponse.json({
+        statusCode: 400,
+        error: {
+          message: "Something went wrong. Please try again",
+        },
+        data: null,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json({
+      statusCode: 500,
+      error: {
+        message: "Something went wrong. Please try again",
+      },
+      data: null,
+    });
+  }
+}
