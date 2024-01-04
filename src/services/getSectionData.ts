dbe18f23a4075e4c339a8a06d96f123b5a80f7f6
Jeremias Villane <vil.jeremias@gmail.com>
2024-01-04 15:37:55 -0300

    refactor(getSectionData): set API_URL const

--- src/services/getSectionData.ts
+++ src/services/getSectionData.ts
@@ -1,12 +1,14 @@
 export async function getSectionData(currentSection: string) {
+  const API_URL =
+    process?.env?.NEXT_PUBLIC_API_URL ??
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ??
+    "http://localhost:3000/";
+
   try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_APP_URL}/api/${currentSection}`,
-      {
-        method: "GET",
-        cache: "no-store",
-      }
-    );
+    const res = await fetch(`${API_URL}/api/${currentSection}`, {
+      method: "GET",
+      cache: "no-store",
+    });
 
     const { data } = await res.json();
 
