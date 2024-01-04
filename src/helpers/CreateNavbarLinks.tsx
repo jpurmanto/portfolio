bf362c2256ad01f335e3089baba302fd91a0bf2e
Jeremias Villane <vil.jeremias@gmail.com>
2024-01-03 22:49:17 -0300

    refactor: wrapping navbar item in li tags

--- src/helpers/CreateNavbarLinks.tsx
+++ src/helpers/CreateNavbarLinks.tsx
@@ -14,27 +14,27 @@ export function CreateNavbarLinks({
   setShowSidebar,
 }: Props): JSX.Element[] {
   return navbarItems.map((item, index) => (
-    <LinkScroll
-      as="li"
-      key={index}
-      activeClass="active"
-      to={item}
-      spy={true}
-      smooth={true}
-      duration={1000}
-      onSetActive={() => {
-        setActiveLink(item);
-        setShowSidebar && setShowSidebar(false);
-      }}
-      className={`px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative
+    <li key={index}>
+      <LinkScroll
+        activeClass="active"
+        to={item}
+        spy={true}
+        smooth={true}
+        duration={1000}
+        onSetActive={() => {
+          setActiveLink(item);
+          setShowSidebar && setShowSidebar(false);
+        }}
+        className={`px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative
     ${
       activeLink === item
         ? "text-[var(--primary-color)] font-bold animation-active shadow-[var(--primary-color)]"
         : "text-[#000] font-bold hover:text-[var(--primary-color)]"
     }
     `}
-    >
-      {item.charAt(0).toUpperCase() + item.slice(1)}
-    </LinkScroll>
+      >
+        {item.charAt(0).toUpperCase() + item.slice(1)}
+      </LinkScroll>
+    </li>
   ));
 }
